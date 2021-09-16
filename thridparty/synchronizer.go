d3c2f1a5a112ba6e00bf74475a372c9eb30cbb16
moli <molix2@outlook.com>
2021-09-16 22:15:30 +0800

    update: 备注

--- thridparty/synchronizer.go
+++ thridparty/synchronizer.go
@@ -22,56 +22,56 @@ type ThridPartyDepartment struct {
 type InjectIncrementUserCallbackFunc func(puller ThridPartyPuller, users []*ThridPartyUser, depts []*ThridPartyDepartment) error
 type InjectPullActionCallbackFunc func(puller ThridPartyPuller) error
 
-// 拉取用户信息
+// ThridPartyUserPuller 拉取用户信息
 type ThridPartyUserPuller interface {
-	// 拉取的用户 PrimaryAttrs 字段，该字段将作为用户的主键进行唯一性匹配
+	// UserPrimaryAttrs 拉取的用户 PrimaryAttrs 字段，该字段将作为用户的主键进行唯一性匹配
 	// 必须返回 PullUsers() 中拥有的字段名，否则会出现错误
 	// 这里返回数组的原因是，允许使用多个字段进行组合主键，通常返回一个即可，例如微信的openid
 	UserPrimaryAttrs() []string
-	// 拉取用户
+	// PullUsers 拉取用户
 	PullUsers() ([]*ThridPartyUser, error)
 
-	// 拉取的部门 Primary 字段，该字段将作为部门的主键进行唯一性匹配
+	// DepartmentPrimaryAttr 拉取的部门 Primary 字段，该字段将作为部门的主键进行唯一性匹配
 	// 必须返回 PullDepts() 中拥有的字段名，否则会出现错误
 	DepartmentPrimaryAttr() string
-	// 拉取部门
+	// PullDepts 拉取部门
 	PullDepts() ([]*ThridPartyDepartment, error)
 
-	// 是否支持增量拉取
+	// HasIncrement 是否支持增量拉取
 	// 对于像微信、钉钉、飞书等支持增量拉取的第三方，可以返回true
 	HasIncrement() bool
-	// 当有增量信息时，调用fn传递给Synchronizer
+	// InjectPullIncrementCallback 当有增量信息时，调用fn传递给Synchronizer
 	InjectPullIncrementCallback(fn InjectIncrementUserCallbackFunc) error
 
-	// 用户触发拉取
+	// InjectPullActionFunc 用户触发拉取
 	InjectPullActionFunc(InjectPullActionCallbackFunc) error
 }
 
-// 三方用户过滤器
+// ThridPartyUserFilter 三方用户过滤器
 // TODO 实现默认过滤器（仅进行部门过滤）
 type ThridPartyUserFilter interface {
-	// 是否有过滤器（如果没有过滤器，则返回false）
+	// HasFilter 是否有过滤器（如果没有过滤器，则返回false）
 	HasFilter() bool
-	// 过滤规则
+	// Filter 过滤规则
 	Filter([]*ThridPartyUser, []*ThridPartyDepartment) ([]*ThridPartyUser, []*ThridPartyDepartment, error)
 }
 
-// 三方拉取器
+// ThridPartyPuller 三方拉取器
 type ThridPartyPuller interface {
-	// 三方唯一性标识
+	// Label 三方唯一性标识
 	Label() string
-	// 获取三方的拉取能力
+	// GetPuller 获取三方的拉取能力
 	GetPuller() ThridPartyUserPuller
-	// 获取三方的过滤器
+	// GetFilter 获取三方的过滤器
 	GetFilter() ThridPartyUserFilter
-	// 定时任务
+	// Cron 定时任务
 	// true表示需要定时任务，并返回定时任务的时间cron表达式
 	// false表示不需要定时任务
 	Cron() (bool, string)
 }
 
 type Synchronizer interface {
-	// 注册三方同步器
+	// RegisterPuller 注册三方同步器
 	// 当有三方同步器注册时，会调用该方法
 	// 如果返回错误，则表示该三方同步器重复注册
 	RegisterPuller(puller ThridPartyPuller) error
