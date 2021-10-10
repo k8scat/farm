2e097f9cbe6c191a18cad1cc997ec33c42274a3c
moli <molix2@outlook.com>
2021-10-11 02:01:43 +0800

    stash

--- exchange/mq_db.go
+++ exchange/mq_db.go
@@ -5,6 +5,8 @@ import (
 	"runtime/debug"
 	"sync"
 	"time"
+
+	"github.com/reactivex/rxgo/v2"
 )
 
 // MQDB is a message queue is based on database
@@ -21,18 +23,18 @@ const (
 type MQDB struct {
 	pipe chan *PipeEvent
 
-	// 当需要从数据库读取event时，该channel能读到数据
-	act chan struct{}
+	// 当需要从数据库读取event时，该channel会被触发
+	act chan rxgo.Item
 
 	subWG       sync.Mutex
-	subscribers map[Action][]Subscriber
+	subscribers map[string]Subscriber
 }
 
 func NewMQDB() *MQDB {
 	mq := new(MQDB)
-	mq.subscribers = make(map[Action][]Subscriber)
+	mq.subscribers = make(map[string]Subscriber)
 	mq.pipe = make(chan *PipeEvent, 1)
-	mq.act = make(chan struct{}, 1)
+	mq.act = make(chan rxgo.Item, 128)
 	return mq
 }
 
@@ -45,12 +47,19 @@ func (d *MQDB) Run() error {
 	// TODO 从数据库中读取 Subscribers 的offset并初始化
 	// TODO 触发从数据库读取Event的工作
 	//	 TODO 根据 subscribers 从数据库中以流的方式读取Event，并推送rxgo.Pipe，超时未处理或重试n次后未被处理的event将被放弃
+	select {
+	case <-rxgo.FromChannel(d.act).Last().Observe():
+
+	}
+
 	return nil
 }
 
 func (d *MQDB) Push(e *Event) error {
 	// TODO 推送到数据库
-	// TODO 触发从数据库读取Event的工作
+	//
+	// 触发从数据库读取Event的工作
+	d.act <- rxgo.Item{}
 	return nil
 }
 
@@ -59,12 +68,17 @@ func (d *MQDB) Subscribers(subs ...Subscriber) {
 	defer d.subWG.Unlock()
 
 	for _, sub := range subs {
-		for _, action := range sub.Actions() {
-			d.subscribers[action] = append(d.subscribers[action], sub)
-		}
+		d.subscribers[sub.Label()] = sub
 	}
 }
 
 func (d *MQDB) Pipe() *PipeEvent {
 	return <-d.pipe
+}
+
+func (d *MQDB) ReadStreamOfEvent() {
+	for _, sub := range d.subscribers {
+		sub.LastOffset()
+		// TODO
+	}
 }
